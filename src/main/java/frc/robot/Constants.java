bbc5d7314ab32c072e6ecee8230948d8abffb76f
Nhuy Nguyen <nhuy.nguyen20076@gmail.com>
2022-05-07 11:57:03 -0400

    added command and arcade drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean
@@ -30,5 +32,8 @@ public final class Constants {
     // controller
     public static final int leftAxis = 1;
     public static final int rightAxis = 5;
+
+    // xbox controller
+    public static final int xboxID = 0;
 
 }
