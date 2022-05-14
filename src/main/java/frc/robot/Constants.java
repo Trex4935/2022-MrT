0e62280d6053920c32a9a1c9fcc044af88105838
Anthrak <alexander.rehrig@gmail.com>
2022-05-14 12:27:30 -0400

    added motors for intake and speed for launcher

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,4 +42,9 @@ public final class Constants {
     public static final int rightInvert = -1;
 
     public static final double motorSpeedMultiplier = 0.75;
+	
+    // launcher motors
+    public static final int motorTop = 3;
+    public static final int motorBottom = 4;
+    public static final double launcherSpeed = 0.725;
 }
