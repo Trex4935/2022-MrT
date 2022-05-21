597dad285ea02eca23cb55221dc367c4ad0a15d1
Nhuy Nguyen <nhuy.nguyen20076@gmail.com>
2022-05-21 13:16:06 -0400

    added right can ids

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,10 +22,10 @@ public final class Constants {
 
     // drive train
 
-    public static final int leftTopCanID = 1;
-    public static final int leftBottomCanID = 11;
-    public static final int rightTopCanID = 2;
-    public static final int rightBottomCanID = 14;
+    public static final int leftTopCanID = 1;// 1;
+    public static final int leftBottomCanID = 2;// 11;
+    public static final int rightTopCanID = 5;// 2;
+    public static final int rightBottomCanID = 6;// 14;
 
     // controller
     public static final int leftAxis = 1;
